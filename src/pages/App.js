08214858a284971cbd3327c4a0dffcb9d28afa8d
Afrizal <afrizalbagas.03@gmail.com>
2022-09-14 22:52:05 +0700

    Add New Screen

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,21 +6,26 @@
  * @flow
  */
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import  Ionicons  from 'react-native-vector-icons/Ionicons';
+import {Button, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Provider} from 'react-redux';
 
-import Splashscreen from "./Splashscreen.js";
-import TabHome from "./TabHome.js";
-import TabSettings from "./TabSettings.js";
-import TabLightingLists from "./TabLightingLists.js";
-import TabReports from "./TabReports.js";
-import Details from "./Details.js";
-import ReportDetails from "./ReportDetails.js";
-import Main from "./Main.js";
+import store from '../redux/store';
 
+import Splashscreen from './Splashscreen.js';
+import TabHome from './TabHome.js';
+import TabSettings from './TabSettings.js';
+import TabLightingLists from './TabLightingLists.js';
+import TabReports from './TabReports.js';
+import Details from './Details.js';
+import ReportDetails from './ReportDetails.js';
+import Main from './Main.js';
+import Login from './Login';
+import Profile from './Profile';
+import UpdateProfile from './UpdateProfile';
 
 const Stack = createStackNavigator();
 
@@ -28,67 +33,95 @@ const Tab = createBottomTabNavigator();
 
 function TabScreen() {
   return (
-	  <Tab.Navigator 
-	  tabBarOptions={{
-		activeTintColor: '#0B108C',
-    inactiveTintColor: '#9DA8C1',
-	  }}>
-        <Tab.Screen
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#0B108C',
+        inactiveTintColor: '#9DA8C1',
+      }}>
+      <Tab.Screen
         name="Home"
         component={TabHome}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
+          tabBarIcon: ({color, size}) => (
             <Ionicons name="ios-home" color={color} size={size} />
           ),
-        }}/>
-		<Tab.Screen
+        }}
+      />
+      <Tab.Screen
         name="LightingLists"
         component={TabLightingLists}
         options={{
           tabBarLabel: 'Lighting',
-          tabBarIcon: ({ color, size }) => (
+          tabBarIcon: ({color, size}) => (
             <Ionicons name="ios-bulb" color={color} size={size} />
           ),
-        }}/>
-		<Tab.Screen
+        }}
+      />
+      <Tab.Screen
         name="Reports"
         component={TabReports}
         options={{
           tabBarLabel: 'Problems',
-          tabBarIcon: ({ color, size }) => (
+          tabBarIcon: ({color, size}) => (
             <Ionicons name="ios-hammer" color={color} size={size} />
           ),
-        }}/>
-    <Tab.Screen name="Settings" component={TabSettings} 
-		options={{
-			tabBarLabel: 'Settings',
-			tabBarIcon: ({ color, size }) => (
-			  <Ionicons name="ios-settings" color={color} size={size} />
-			),
-		  }}/>
-      </Tab.Navigator>
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={TabSettings}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="ios-settings" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
 function App() {
   return (
-    <NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Main">
+          {/* Tab Navigation */}
+          <Stack.Screen
+            name="Home"
+            component={TabScreen}
+            options={{headerShown: false}}
+          />
 
-      <Stack.Navigator initialRouteName="Home">
-		
-		{/* Tab Navigation */}
-		<Stack.Screen name="Home" component={TabScreen} options={{ headerShown: false }}/>
-
-		{/* Page Navigation */}
-		<Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
-		<Stack.Screen name="Details" component={Details} options={Details.navigationOptions}/>
-		<Stack.Screen name="ReportDetails" component={ReportDetails} options={Details.navigationOptions}/>
-		<Stack.Screen name="Settings" component={TabSettings} />
-		<Stack.Screen name="Splashscreen" component={Splashscreen} options={{ headerShown: false }}/>
-
-      </Stack.Navigator>
-    </NavigationContainer>
+          {/* Page Navigation */}
+          <Stack.Screen
+            name="Main"
+            component={Main}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Details"
+            component={Details}
+            options={Details.navigationOptions}
+          />
+          <Stack.Screen
+            name="ReportDetails"
+            component={ReportDetails}
+            options={Details.navigationOptions}
+          />
+          <Stack.Screen name="Settings" component={TabSettings} />
+          <Stack.Screen
+            name="Splashscreen"
+            component={Splashscreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="UpdateProfile" component={UpdateProfile} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
